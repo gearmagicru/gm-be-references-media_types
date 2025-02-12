086ab62a7fd21b9619f179e08d460199521458a0
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-12 19:52:45 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Media types',
+    '{description}' => 'Media types',
+    '{permissions}' => [
+        'any'  => ['Full access', 'Viewing and making changes to media types'],
+        'view' => ['View', 'Viewing media types'],
+        'read' => ['Read', 'Reading media types']
+    ],
+
+    // Grid: контекстное меню записи
+    'Edit record' => 'Edit record',
+    // Grid: столбцы
+    'Name' => 'Name',
+    'Type' => 'Type',
+    'Icon' => 'Icon',
+
+    // Form
+    '{form.title}' => 'Add media types',
+    '{form.titleTpl}' => 'Edit media types "{name}"'
+];
